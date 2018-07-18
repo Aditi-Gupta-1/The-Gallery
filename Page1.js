2928c9aeda5bb55b74e26d5e97eafcc165ce79b9
Aditi-Gupta-1 <33714906+Aditi-Gupta-1@users.noreply.github.com>
2018-07-18 18:45:20 +0530

    Add files via upload

---
+++ Page1.js
@@ -0,0 +1,13 @@
+function check(form){
+	if(form.userid.value=="user" && form.pswrd.value=="paaas"){
+		window.open('page2.html')
+	}
+	else{
+		alert("Wrong username or password")
+	}
+	}
+
+
+
+
+
